d3c6c83efac6bb50dd1082ed96dd6c45b9db0e22
Umesh Deogharia <143274287+Umesh-Deogharia@users.noreply.github.com>
2024-09-08 09:43:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function toCase(text) {
   // write your code here
+	let str1 = text.toUpperCase();
+	let str2 = text.toLowerCase();
+	return str2.concat(`-${str1}`);
 }
 
 // DO not change the code below
-
-const text = prompt("Enter text:");
+// const text = prompt("Enter text:");
 alert(toCase(text));
